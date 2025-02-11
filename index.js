82227206339a4e4c86508872fa051234016298d1
likhithkp <likhithkpdev@gmail.com>
2025-02-11 11:19:33 +0530

    PRactice

--- index.js
+++ index.js
@@ -9,10 +9,27 @@ class CustomArray{
         this.length++;
         return this.length;
     };
+
+    pop(){
+        if(this.length === 0) return undefined;
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+
+    unshift(item){
+        this.data[0] = item;
+        this.length++;
+    }
+
+    get(index){
+        return this.data[index];
+    }
 } 
 
 const newArr = new CustomArray();
 newArr.push("Shiba");
 newArr.push("Shiba1");
 newArr.push("Shiba2");
-console.log(newArr);
+console.log('Array', newArr);
+newArr.unshift('ShibaFirst');
+console.log('Array1', newArr);
